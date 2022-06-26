ea7d5287778a2c690dbb10a421d45a32815587f8
nem0z <maxime.bernard.dev@gmail.com>
2022-06-26 02:05:32 +0200

    edit gitignore

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-export default {
-    "API_KEY": "22c537dcc89874d07780141379da4f20",
-    "API_SECRET": "39gWJPx9d3xBtTWz3IkSTWldN5IcevrsrZL9pDoqI8oTNoZeL9gCTWXaIzOjsBEUz+TZIsGHDSb8lYJFAqnB7g==",
-    "PASSPHRASE": "megltm5ekrh"
-};
